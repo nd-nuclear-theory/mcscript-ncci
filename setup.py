11ae5dd9aaf08f8672268dddec41a80459df1665
Patrick Fasano <patrick@patrickfasano.com>
2017-12-22 17:24:14 -0600

    setup: Remove email

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     name="ncci",
     version="0.0.1",
     author="Mark A. Caprio, Patrick J. Fasano, University of Notre Dame",
-    author_email="Mark Caprio <mcaprio@nd.edu>",
     description=("Scripting for NCCI runs"),
     license="MIT",
     packages=['ncci'],
