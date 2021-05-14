89c3eb7ba836a7877f1fa1afa213f1fd0cb74d54
Patrick Fasano <patrick@patrickfasano.com>
2021-05-14 10:52:38 -0400

    setup: Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mcscript-ncci",
-    version="0.1.0",
+    version="0.1.1",
     author="Mark A. Caprio, Patrick J. Fasano, University of Notre Dame",
     description=("Scripting for NCCI runs"),
     license="MIT",
