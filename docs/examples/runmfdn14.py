4c2f57f1a5107468b74caa6a2d7e90b965bc91b7
Mark A. Caprio <mcaprio-nd-edu>
2023-06-16 15:14:44 -0400

    docs/examples: Remove subdirectory v15-beta02 from executable path

--- docs/examples/runmfdn14.py
+++ docs/examples/runmfdn14.py
@@ -170,7 +170,7 @@ tasks = [
         
         # version parameters
         "h2_format": 15099,
-        "mfdn_executable": "v15-beta02/xmfdn-h2-lan",
+        "mfdn_executable": "xmfdn-h2-lan",
         "mfdn_driver": ncci.mfdn_v15,
     }
     for nuclide in nuclide_list
