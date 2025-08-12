5597de076b8fdaf5d922277c50f6dc8740e25ad9
Scott Baker <sbaker23@nd.edu>
2025-08-12 16:02:45 -0400

    examples: Update counting example and provide example output

---
+++ doc/examples/example-output/runmfdncounting01/runmfdncounting01-mfdn15-Z02-N06-Nmax08-Mj0.0.res
@@ -0,0 +1,32 @@
+
+[MFDn]
+Version = 15
+Revision = v15b01-112-g74922b7
+Platform = 
+Username = 
+ndiags   =        1
+MPIranks  =        1
+OMPthreads =        1
+
+[PARAMETERS]
+
+[Basis]
+Nprotons  =        2
+Nneutrons =        6
+# Orbitals read in from file
+TwoMj  =        0
+parity =        1
+Nmin   =        0
+Nmax   =        8
+DeltaN =        2
+WTmax  =    12.1000
+
+[Diagonalization]
+neivals   =        5
+maxits    =      400
+startit   =        0
+selectpiv =        3
+tol       =     0.0000
+
+[Many-body matrix]
+dimension  =            7463678
--- doc/examples/runmfdncounting01.py
+++ doc/examples/runmfdncounting01.py
@@ -1,4 +1,4 @@
-""" runmfdn10.py
+""" runmfdncounting01.py
 
     See examples/README.md for full description.
 
@@ -8,6 +8,7 @@
     - 09/27/17 (pjf): Created, copied from runmfd07.
     - 12/19/17 (pjf): Update for mfdn->ncci rename.
     - 09/07/19 (pjf): Remove Nv from truncation_parameters.
+    - 08/12/25 (seb): Rename and remove unneeded parameters.
 """
 
 import mcscript
@@ -24,40 +25,13 @@ mcscript.control.init()
 # build task list
 ##################################################################
 
-ncci.environ.interaction_dir_list = [
-    "run0164-JISP16-ob-9",
-    "run0164-JISP16-ob-13",
-    "run0164-JISP16-tb-10",
-    "run0164-JISP16-tb-20",
-    "run0306-N2LOopt500",  # up to tb-20
-    "runvc0083-Daejeon16-ob-13"
-]
-
 tasks = [{
     # nuclide parameters
     "nuclide": (2, 6),
 
-    # Hamiltonian parameters
-    "interaction": "JISP16",
-    "use_coulomb": True,
-    "a_cm": 20.,
-    "hw_cm": None,
-
-    # input TBME parameters
-    "truncation_int": ("tb", 10),
-    "hw_int": 20.,
-    "truncation_coul": ("tb", 10),
-    "hw_coul": 20.,
-
     # basis parameters
     "basis_mode": ncci.modes.BasisMode.kDirect,
     "hw": 20.,
-
-    # transformation parameters
-    "xform_truncation_int": None,
-    "xform_truncation_coul": None,
-    "hw_coul_rescaled": None,
-    "target_truncation": None,
 
     # traditional oscillator many-body truncation
     "sp_truncation_mode": ncci.modes.SingleParticleTruncationMode.kNmax,
@@ -68,31 +42,9 @@ tasks = [{
         "M": 0,
         },
 
-    # diagonalization parameters
-    "diagonalization": True,
-    "eigenvectors": 2,
-    "initial_vector": -2,
-    "max_iterations": 200,
-    "tolerance": 1e-6,
-    "partition_filename": None,
-
-    # obdme parameters
-    ## "hw_for_trans": 20,
-    "obdme_multipolarity": 2,
-    # "obdme_reference_state_list": [(0, 0, 1)],
-    "save_obdme": True,
-
-    # two-body observables
-    ## "tb_observable_sets": ["H-components","am-sqr"],
-    "tb_observable_sets": ["H-components"],
-    "tb_observables": [],
-
-    # wavefunction storage
-    "save_wavefunctions": True,
-
     # version parameters
     "h2_format": 15099,
-    "mfdn_executable": "v15-beta01/xmfdn-h2-lan",
+    "mfdn_executable": "xmfdn-h2-lan",
     "mfdn_driver": ncci.mfdn_v15
 }]
 
